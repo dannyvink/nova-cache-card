8eac770bc7d2725be9149185ba98223861135f30
Danny Vink <daniel.vink@hired.com>
2019-02-16 10:04:55 -0800

    Switch to controller-based routes

--- routes/api.php
+++ routes/api.php
@@ -15,35 +15,6 @@ use Vink\CacheCard\CacheHelpers;
 | as many additional routes to this file as your card may require.
 |
 */
-Route::post('/flush', function (Request $request) {
-    /**
-     * Throw something in the cache to make sure there is a valid connection. This also ensures that
-     * the proper directories have been created for the 'file' cache driver.
-     */
-    Cache::forever('flushing', true);
-    $canCacheSuccessfully = Cache::get('flushing');
-    return [
-        'success' => $canCacheSuccessfully && Cache::flush(),
-        'size' => CacheHelpers::getFileCacheSize()
-    ];
-});
-
-Route::post('/cache', function (Request $request) {
-    $key = $request->get('cacheKey');
-    return [
-        'success' => Cache::forget($key),
-        'key' => htmlentities($key),
-        'size' => CacheHelpers::getFileCacheSize()
-    ];
-});
-
-Route::get('/cache', function (Request $request) {
-    $key = $request->get('cacheKey');
-    $result = Cache::get($key);
-    return [
-        'success' => !!$result,
-        'key' => htmlentities($key),
-        'value' => $result,
-        'size' => CacheHelpers::getFileCacheSize()
-    ];
-});
+Route::post('/flush', 'Vink\CacheCard\Http\Controllers\CacheCardController@flush');
+Route::post('/cache', 'Vink\CacheCard\Http\Controllers\CacheCardController@forget');
+Route::get('/cache', 'Vink\CacheCard\Http\Controllers\CacheCardController@get');
